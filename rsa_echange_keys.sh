374e3587687eb9ad42526d67d28597694c3b1203
nicolas <nicolas@niocs.me>
2018-10-11 14:25:37 +0200

    Update rsa_echange_keys.sh

--- rsa_echange_keys.sh
+++ rsa_echange_keys.sh
@@ -18,24 +18,24 @@ if [[ $# -eq 0 ]] ; then
     echo 'mot de passe'
     read password
 fi
-sshpass -p $password ssh -p $3 $BACKUPUSER@$PC_TARGET 'test -d $HOME/.ssh'
+sshpass -p $password ssh -p $port_ssh $BACKUPUSER@$PC_TARGET 'test -d $HOME/.ssh'
 R=$?
 
 if [ $R = 0 ]
 	then
 		echo "OK d"
-		sshpass -p $password ssh -p $3 $BACKUPUSER@$PC_TARGET 'test -f $HOME/.ssh/authorized_keys'
+		sshpass -p $password ssh -p $port_ssh $BACKUPUSER@$PC_TARGET 'test -f $HOME/.ssh/authorized_keys'
 		R2=$?
 		if [ $R2 = 0 ]
 			then
 				echo "OK f"
 			else
 				echo "création du fichier"
-				sshpass -p $password ssh -p $3 $BACKUPUSER@$PC_TARGET 'touch $HOME/.ssh/authorized_keys'
+				sshpass -p $password ssh -p $port_ssh $BACKUPUSER@$PC_TARGET 'touch $HOME/.ssh/authorized_keys'
 		fi
 	else
 		echo "création du repertoire et du fichier"
-		sshpass -p $password ssh -p $3 $BACKUPUSER@$PC_TARGET 'mkdir -p $HOME/.ssh'
-		sshpass -p $password ssh -p $3 $BACKUPUSER@$PC_TARGET 'touch $HOME/.ssh/authorized_keys'
+		sshpass -p $password ssh -p $port_ssh $BACKUPUSER@$PC_TARGET 'mkdir -p $HOME/.ssh'
+		sshpass -p $password ssh -p $port_ssh $BACKUPUSER@$PC_TARGET 'touch $HOME/.ssh/authorized_keys'
 fi
-cat $HOME/.ssh/id_rsa.pub|sshpass -p $password ssh -p $3 $BACKUPUSER@$PC_TARGET "cat >> .ssh/authorized_keys ; chmod 600 .ssh/authorized_keys"
+cat $HOME/.ssh/id_rsa.pub|sshpass -p $password ssh -p $port_ssh $BACKUPUSER@$PC_TARGET "cat >> .ssh/authorized_keys ; chmod 600 .ssh/authorized_keys"
